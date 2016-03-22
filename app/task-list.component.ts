89f34a874ca8d46b05222a8e5e0961c5918fa155
Blake Scott <blakerscott@gmail.com>
2016-03-22 16:12:00 -0700

    remove TaskListComponent from app.component file and put it in its own file

---
+++ app/task-list.component.ts
@@ -0,0 +1,26 @@
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
+  template: `
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
