595f52a24e793bc2e482c820ca0f79c2894c3128
Blake Scott <blakerscott@gmail.com>
2016-03-22 13:44:56 -0700

    add a click event emitter to the outputted tasks to that the object prints to the console

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div *ngFor="#task of tasks">
-<h3>{{ task.description }}</h3>
-<h4> Hi, I'm another nested HTML element. There are 4 copies of me because we have 4 tasks.</h4>
-</div>
+    <div class="container">
+      <h1>To-Do List</h1>
+      <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+        {{ task.description }}
+      </h3>
+    </div>
   `
 })
 export class AppComponent {
-  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  public tasks: Task[];
   constructor(){
     this.tasks = [
       new Task("Create To-Do List app.", 0),
@@ -18,6 +20,9 @@ export class AppComponent {
       new Task("Rewatch all the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
     ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
   }
 }
 
